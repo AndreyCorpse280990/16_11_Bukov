6d89dfdc9e847da2ccd1490e28d2936f4a4e82c4
AndreyCorpse28090 <280990corpse@gmail.com>
2023-11-30 18:27:45 +0300

    Добавьте файлы проекта.

---
+++ 16_11_Bukov/16_11_Bukov.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+int main() 
+{
+    std::string num = "123";
+
+    auto is_num = [](const std::string& str) 
+        {
+        for (char c : str) 
+        {
+            if (!std::isdigit(c))
+            {
+                return false;
+            }
+        }
+        return true;
+        };
+
+    if (is_num(num))
+    {
+        std::cout << "It's a number." << std::endl;
+    }
+    else 
+    {
+        std::cout << "It's not a number." << std::endl;
+    }
+
+    return 0;
+}
